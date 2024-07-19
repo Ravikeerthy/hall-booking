1f897f3f22acd2a1dc3e77c6f5a690176fa2f713
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-07-19 19:42:39 +0400

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import hallBookingRouter from "./Router/hallBookingRouter.js";
+import express from 'express';
+import cors from 'cors';
+import hallBookingRouter from './Router/hallBookingRouter.js'
 
 const app = express();
 app.use(express.json());
@@ -8,41 +8,12 @@ app.use(cors());
 
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.status(200)
-    .json(`<div style="text-align: center; background-color:lightblue;  padding: 10px;"><h1>Welcome to Our Hall Booking Page</h1></div>
-        <div>
-        <ul>
-        <li>
-        <h3><mark>Get:</mark> Use the endpoint to <mark>/api/hallbooking/getDetails</mark> to get the All Room Details</h3>
-        </li>
-    
-        <li>
-        <h3><mark>Post:</mark> Use the endpoint to <mark>/api/hallbooking/newRoom</mark> to Create the New Room</h3>
-        </li>
-    
-        <li>
-        <h3><mark>Get:</mark> Use the endpoint to <mark>/api/hallbooking/newbooking</mark> to Book a New Room</h3>
-        </li>
-    
-        <li>
-        <h3><mark>Get:</mark> Change the endpoint to <mark>/api/hallbooking/bookedRoomDetails</mark> to retrieve Booked Room Data</h3>
-        </li>
-    
-        <li>
-        <h3><mark>Get:</mark> Change the endpoint to <mark>/api/hallbooking/customerBookedDetails</mark> to retrieve Data of Booked Customers and Rooms</h3>
-        </li>
-    
-        <li>
-        <h3><mark>Get:</mark> Change the endpoint to <mark>/api/hallbooking/cusbookcount</mark> to retrieve Booking Counts and Room Data for Booked Customers</h3>
-        </li>
-    
-        </ul> 
-        </div>`);
-});
+app.get('/',(req, res)=>{
+    res.status(200).json({message:"Welcome to Our Party's Hall Booking"})
+})
 
-app.use("/api/hallbooking", hallBookingRouter);
+app.use('/api/hallbooking', hallBookingRouter);
 
-app.listen(port, () => {
-  console.log("App is Listening:", port);
-});
+
+
+app.listen(port, ()=>{console.log("App is Listening:", port);})
